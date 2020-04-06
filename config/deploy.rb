d87029e20829e94bdf24b5c7aeeac8f57146e4f8
kota kanazawa <kota0818@gmail.com>
2020-04-06 21:12:15 +0900

    test

---
+++ config/deploy.rb
@@ -0,0 +1,13 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.12.1"
+
+set :application, "capistrano_psql"
+set :repo_url, "git@github.com:kotakanazawa/capistrano_psql.git"
+set :user, "deploy"
+
+set :rbenv_type, :user
+set :rbenv_ruby, File.read('.ruby-version').strip
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+
+append :linked_dirs, '.bundle'
+set :bundle_jobs, 2
